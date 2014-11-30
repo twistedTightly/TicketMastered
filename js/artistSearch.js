b7d3fabcf8edaf82612b8a942ada4d2838cf599e
twistedTightly <mrauh1@nd.edu>
2014-11-30 00:14:59 -0500

    Continue, skip to results for location added, other fixes

--- js/artistSearch.js
+++ js/artistSearch.js
@@ -47,32 +47,63 @@ $('#the-basics .typeahead').typeahead({
 });
 
 
+/* This function is called whenever an artist is selected */
+var artistSelected = function() {
+	$('#artistSelectionSection #skipToResults').css('display', 'block');
+	$('#artistSelectionSection .next').html('Continue ' + '<div class="glyphicon glyphicon-chevron-down" aria-hidden="true"></div>');
+	// Adjust the button's positioning, which is dependent on its width
+	/*var leftMargin = $('#artistSelectionSection .next').outerWidth() / 2 * -1;
+	$('#artistSelectionSection .next').css('margin-left', leftMargin + 'px');*/
+
+	// Add the artist info section
+	$('#artistInfo').append('<img alt="Maroon 5" src="images/homepage/maroon5-circle_200x200.png"><h1>Maroon 5</h1>');
+ };
+
+ var locationSelected = function() {
+ 	$('#locationSelection #skipToResults').css('display', 'block');
+	$('#locationSelection .next').html('Continue ' + '<div class="glyphicon glyphicon-chevron-down" aria-hidden="true"></div>');
+
+	// TODO: Drop pin on map
+ };
+
  $(document).ready(function(){
  	// When an artist is selected from the list, the artist's info
  	// is dynamically displayed below the search bar.
- 	$('.tt-dataset-artists').click(function() {
- 		$('#skipToResults').css('display', 'block');
- 		$('#artistSelectionSection .next').html('Continue' + '<div class="glyphicon glyphicon-chevron-down" aria-hidden="true"></div>');
- 		// Adjust the button's positioning, which is dependent on its width
- 		/*var leftMargin = $('#artistSelectionSection .next').outerWidth() / 2 * -1;
- 		$('#artistSelectionSection .next').css('margin-left', leftMargin + 'px');*/
- 	});
+ 	$('.tt-dataset-artists').click(artistSelected);
+ 	// This triggers the UI changes if the enter button is pressed on the
+ 	// artist input box
+ 	$('#artistNameInput').keyup(function(event){
+	    if(event.keyCode === 13){
+	        artistSelected();
+	    }
+	});
 
- 	// Arist skip button
+	// When a location is selected
+ 	// TODO: When input is selected from maps suggestions
+ 	// This triggers the UI changes if the enter button is pressed on the
+ 	// location input box
+ 	/*$('#locationInput').keyup(function(event){
+	    if(event.keyCode === 13){
+	        locationSelected();
+	    }
+	});*/
+
+ 	// Positioning the arist skip button
  	$('#artistSelectionSection .next').affix({
  		// The extra 50 at the end corresponds to the top margin of the locationSelection div
 		offset: {
-			bottom: $(document).height() - $('#artistSelectionSection').outerHeight()
+			bottom: $(document).height() - $('nav').outerHeight() - $('#artistSelectionSection').outerHeight()
 				- $('#artistSelectionSection .next').outerHeight() - 50
 		}
 
 	});
 
-	// Location skip button
+	// Positioning the location skip button
  	$('#locationSelection .next').affix({
 		offset: {
-			bottom: $(document).height() - $('#artistSelectionSection').outerHeight()
-				- $('#locationSelection').outerHeight() - $('#locationSelection .next').outerHeight() - 50
+			top: $('nav').outerHeight() + $('#artistSelectionSection').outerHeight() + $('#locationSearch').outerHeight(),
+			bottom: $(document).height() - $('nav').outerHeight() - $('#artistSelectionSection').outerHeight()
+				- $('#locationSelection').outerHeight() - $('#locationSelection .next').outerHeight()
 		}
 	});
 
