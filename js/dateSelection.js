689bf85f5ceea9125b2da933f961ea20954ca71e
twistedTightly <mrauh1@nd.edu>
2014-11-30 16:30:21 -0500

    Calendar buttons updated, can select range of dates

---
+++ js/dateSelection.js
@@ -0,0 +1,10 @@
+/* dateSelection.js
+ *
+ * This file contains all functions to style and provide
+ * backend functionality to the date selection section
+ * of the main page.
+ */
+
+ $(document).ready(function(){
+ 	
+ });
--- js/datepicker.js
+++ js/datepicker.js
@@ -1,10 +1,4 @@
 /*
- * Initializes the date picker
- */
-
-$( "#datepicker" ).datepicker();
-
-/*
  * Allows multiple date selection
  * 
  * Reference: http://stackoverflow.com/questions/1452066/jquery-ui-datepicker-multiple-date-selections
@@ -23,12 +17,14 @@ function removeDate(index) {
 }
 
 // Adds a date if we don't have it yet, else remove it
-function addOrRemoveDate(date) {
+function addOrRemoveDate(date, inst) {
+    //console.log(inst);
     var index = jQuery.inArray(date, dates);
     if (index >= 0) 
         removeDate(index);
     else 
         addDate(date);
+    console.log(dates);
 }
 
 // Takes a 1-digit number and inserts a zero before it
@@ -39,26 +35,30 @@ function padNumber(number) {
     return ret;
 }
 
-jQuery(function () {
-    jQuery("#datepicker").datepicker({
-        onSelect: function (dateText, inst) {
-            addOrRemoveDate(dateText);
-        },
-        beforeShowDay: function (date) {
-            var year = date.getFullYear();
-            // months and days are inserted into the array in the form, e.g "01/01/2009", but here the format is "1/1/2009"
-            var month = padNumber(date.getMonth() + 1);
-            var day = padNumber(date.getDate());
-            // This depends on the datepicker's date format
-            var dateString = month + "/" + day + "/" + year;
+/*
+ * Initializes the date picker
+ */
+$("#datepicker").datepicker({
+    onSelect: function (dateText, inst) {
+        addOrRemoveDate(dateText, inst);
+        var month = dateText.split('/')[0];
+    },
+    beforeShowDay: function (date) {
+        var year = date.getFullYear();
+        // months and days are inserted into the array in the form, e.g "01/01/2009", but here the format is "1/1/2009"
+        var month = padNumber(date.getMonth() + 1);
+        var day = padNumber(date.getDate());
+        // This depends on the datepicker's date format
+        var dateString = month + "/" + day + "/" + year;
 
-            var gotDate = jQuery.inArray(dateString, dates);
-            if (gotDate >= 0) {
-                // Enable date so it can be deselected. Set style to be highlighted
-                return [true, "ui-state-highlight"];
-            }
-            // Dates not in the array are left enabled, but with no extra style
-            return [true, ""];
+        var gotDate = jQuery.inArray(dateString, dates);
+        if (gotDate >= 0) {
+            // Enable date so it can be deselected. Set style to be highlighted
+            return [true, "ui-state-highlight"];
         }
-    });
+        // Dates not in the array are left enabled, but with no extra style
+        return [true, ""];
+    },
+    nextText: '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>',
+    prevText: '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>'
 });
