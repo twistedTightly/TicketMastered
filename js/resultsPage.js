fda1a3a9aa0b96f11396c299d7a93a3bec2a4800
twistedTightly <mrauh1@nd.edu>
2014-12-04 20:18:07 -0500

    More tweaks

---
+++ js/resultsPage.js
@@ -0,0 +1,9 @@
+/* resultsPage.js
+ *
+ * Javascript for random bits of the results page
+ */
+
+
+function notImplementedAlert() {
+    return confirm("This functionality is not currently implemented. We apologize for the inconvenience.");
+}
